4b8e78b4d9d487d3d0f1f944ef5d5f2df9b663e0
Laverde2723 <juanklaverde03@gmail.com>
2022-04-05 15:25:20 -0500

    Doceavo commit

--- sketch.js
+++ sketch.js
@@ -1,10 +1,17 @@
 let imagenes;
-let pantalla = 3;
+let pantalla = 1;
 let songs;
 let slider;
 
 function preload(){
-  songs = loadSound("./Songs/Training.mp3");
+  songs = loadSound("./Songs/SiVeoATuMama.mp3");
+  songs = loadSound("./Songs/258.mp3");
+  songs = loadSound("./Songs/Bichiyal.mp3");
+  songs = loadSound("./Songs/LaZona.mp3");
+  songs = loadSound("./Songs/120.mp3");
+  songs = loadSound("./Songs/YoVistoAsi.mp3");
+  songs = loadSound("./Songs/HaciendoQueMeAmas.mp3");
+  songs = loadSound("./Songs/TeDeseoLoMejor.mp3");
 }
 
 
@@ -12,8 +19,10 @@ function setup() {
   createCanvas(1220, 720);
   imagenes = new Imagenes(pantalla);
   slider = createSlider(0, 1, 0, 0.01);
+  slider.position(1000,650);
   songs.stop();
 }
+
  
 function draw() {
   background(0);
@@ -27,4 +36,5 @@ function draw() {
 
 function mousePressed(){
   imagenes.click(mouseX, mouseY);
+
 }
